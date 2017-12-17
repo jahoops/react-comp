fb23488ce2240fa1d1875995bdab5baf178e675f
OldDad <jwhooper@gmail.com>
2017-12-17 17:23:16 -0500

    mo

--- src/DateRange.jsx
+++ src/DateRange.jsx
@@ -1,15 +1,33 @@
 'use strict';
 
 import React from 'react';
+import AlertContainer from 'react-alert'
 
-class DateRange extends React.Component {
-  constructor(props) {
-    super(props);
+export default class DateRange extends React.Component {
+  alertOptions = {
+    offset: 14,
+    position: 'bottom left',
+    theme: 'dark',
+    time: 5000,
+    transition: 'scale'
+  };
+
+  showAlert = () => {
+    this.msg.show('Some text or component', {
+      time: 2000,
+      type: 'success',
+      icon: <img src="path/to/some/img/32x32.png" />
+    });
   }
+
   render() {
-    return <div>{this.props.text}</div>;
+    return (
+      <div>
+        <AlertContainer ref={a => this.msg = a} {...this.alertOptions} />
+        <button onClick={this.showAlert}>Show Alert</button>
+      </div>
+    );
   }
 
 };
 
-export default DateRange;
